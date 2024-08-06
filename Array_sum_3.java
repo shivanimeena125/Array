5e6aa203ec211c9043c83b7ac527b6c337b30b0b
shivanimeena125 <shivanimeena7454@gmail.com>
2024-08-06 14:14:59 +0530

    Add files via upload

---
+++ Array_sum_3.java
@@ -0,0 +1,17 @@
+class Array_3 {
+    public static void main(String[] args) {
+        int[] arr={12,15,6,21,8};
+        // System.out.println(arr.length);
+        int[]arr1=new int[5];
+        int sum=0;
+        int x=0;
+        for(int i=0;i<arr.length;i++){
+            arr1[x]=arr[i];
+            x++;
+    sum=sum+arr1[i];
+           
+        } System.out.println(sum);
+    }
+}
+
+
