602238e8836d0111d36722a3b2201428a29c3e17
shivanimeena125 <shivanimeena7454@gmail.com>
2024-09-02 21:29:43 +0530

    Add files via upload

---
+++ ArrayNum.java
@@ -0,0 +1,24 @@
+public class ArrayNum {
+    public static void main(String[] args) {
+                        //create array with number 
+        //  int []arr={1,2,3,4,5};
+        // for(int i=0;i<arr.length;i++)
+        // System.out.println(arr[i]);
+        
+                    //access the frist element of the array 
+        // int arr[]={10,20,30,40,50};
+        // int a=arr.length;
+        //     System.out.println(arr[0]);
+
+                    //last element of the array
+        // int []ar={5,10,15,20,25};
+        // int l=ar.length-1;
+        // System.out.println(ar[l]); 
+
+
+                        //lenght of the array
+        int ar[]={7,14,21,28,35};
+        System.out.println(ar.length);
+        
+    }
+}
